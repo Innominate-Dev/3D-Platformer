51c3b4cb735a15db1f61f6ca35da0d1aca5a13fe
Innominate-Dev <InnominateDev@gmail.com>
2022-11-16 12:45:47 +0000

    FIXED Fractured Vent
    - Made the vent break upon touching the player

--- Assets/Scripts/FracturedObjects.cs
+++ Assets/Scripts/FracturedObjects.cs
@@ -4,11 +4,21 @@ using UnityEngine;
 
 public class FracturedObjects : MonoBehaviour
 {
+    Rigidbody[] rbs;
+
     private void OnCollisionEnter(Collision collision)
     {
         if(collision.gameObject.tag == "Player")
         {
+            Debug.Log("collided");
 
+            rbs = GetComponentsInChildren<Rigidbody>();
+
+            for (int i = 0; i < rbs.Length; i++)
+            {
+                rbs[i].isKinematic = false;
+                rbs[i].useGravity = true;
+            }
         }
     }
 }
